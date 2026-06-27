0fe98ceb62c52ab2395a835dfcf41b14c650eedd
Soumya <sowmyah871@gmail.com>
2026-06-27 18:57:27 +0530

    Add files via upload

---
+++ app/data/stock_metadata.py
@@ -0,0 +1,32 @@
+STOCK_METADATA = {
+    "Infosys": {
+        "sector": "IT",
+        "ticker":"INFY.NS"
+    },
+
+    "TCS": {
+        "sector": "IT",
+        "ticker":"TCS.NS"
+    },
+
+    "Wipro": {
+        "sector": "IT"
+    },
+
+    "HDFC": {
+        "sector": "Banking"
+    },
+
+    "ICICI": {
+        "sector": "Banking"
+    },
+
+    "ITC": {
+        "sector": "Consumer",
+        "ticker":"ITC.NS"
+    },
+
+    "Reliance": {
+        "sector": "Energy"
+    }
+}
